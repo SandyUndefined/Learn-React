fe33bf47aab958d9cb97586062f1f384a084a814
Sandy_undefined <08sandysk@gmail.com>
2020-12-23 13:08:17 +0530

    Setup and nested components

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Nested Components
+function ListItems() {
+  return (
+    <section>
+      <ItemName />
+      <Image />
+      <Price />
+    </section>
+  );
+}
+
+const ItemName = () => {
+  return <article>Item</article>;
+};
+const Image = () => (
+  <img src="https://rukminim1.flixcart.com/image/150/150/k0463rk0/monitor/n/6/b/v206hql-v206hql-acer-original-imafjzemapccwgby.jpeg?q=70"></img>
 );
+const Price = () => <p>555.50</p>;
+// Components
+// const Name = () => <p>Sandeep Sharma</p>;
+// const Message = () => {
+//   return <p>To my world</p>;
+// };
+// const Welcome = () => {
+//   return React.createElement("h1", {}, "Learn React");
+// };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<ListItems />, document.getElementById("root"));
