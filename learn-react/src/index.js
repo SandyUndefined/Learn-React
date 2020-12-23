e8d25cc612f9244ce7cd2274f017da92f7d95f13
Sandy_undefined <08sandysk@gmail.com>
2020-12-23 15:45:34 +0530

    Added csss

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,24 +1,47 @@
 import React from "react";
 import ReactDom from "react-dom";
+// css
+import "./index.css";
 
 // Nested Components
 function ListItems() {
   return (
-    <section>
-      <ItemName />
-      <Image />
-      <Price />
+    <section className="itemList">
+      <Items />
+      <Items />
+      <Items />
+      <Items />
+      <Items />
+      <Items />
+      <Items />
+      <Items />
     </section>
   );
 }
 
+const Items = () => {
+  return (
+    <article className="items">
+      <Image />
+      <ItemName />
+      <Price />
+    </article>
+  );
+};
 const ItemName = () => {
-  return <article>Item</article>;
+  return <h4>Item</h4>;
 };
 const Image = () => (
-  <img src="https://rukminim1.flixcart.com/image/150/150/k0463rk0/monitor/n/6/b/v206hql-v206hql-acer-original-imafjzemapccwgby.jpeg?q=70"></img>
+  <img
+    src="https://rukminim1.flixcart.com/image/150/150/k0463rk0/monitor/n/6/b/v206hql-v206hql-acer-original-imafjzemapccwgby.jpeg?q=70"
+    alt="something"
+  ></img>
 );
-const Price = () => <p>555.50</p>;
+const Price = () => (
+  // Inline-css
+  <p style={{ color: "yellowgreen", marginTop: "0.25rem" }}>Rs:556.50</p>
+);
+
 // Components
 // const Name = () => <p>Sandeep Sharma</p>;
 // const Message = () => {
