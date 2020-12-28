9d7a578fd627ae767eb636464ecbc2f2def4e555
Sandy_undefined <08sandysk@gmail.com>
2020-12-28 16:17:20 +0530

    When to use useSatate

--- Advanced/src/tutorial/1-useState/setup/1-error-example.js
+++ Advanced/src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const title = "Random title";
+  const clickEvent = () => {
+    console.log(title);
+  };
+  return (
+    <React.Fragment>
+      <h3>{title}</h3>
+      <button type="button" className="btn" onClick={clickEvent}>
+        Change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
