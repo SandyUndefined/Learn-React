e967ab52e5b3f74cbd6e8ccb0ad66c2fd83b3ba1
Sandy_undefined <08sandysk@gmail.com>
2020-12-28 17:38:18 +0530

    useState Object

--- Advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ Advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Sandeep",
+    age: 24,
+    description: "Bio here",
+  });
+  const changeMessage = () => {
+    setPerson({ ...person, description: "Insta Bio here!" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.description}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Chnage Bio
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
