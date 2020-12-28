ee09eef3af4403451fd15b8fb538345009fc675e
Sandy_undefined <08sandysk@gmail.com>
2020-12-28 16:42:55 +0530

    useState Basic

--- Advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ Advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [initialName, setName] = useState("SandyUndefined");
+  const clickEvent = () => {
+    setName("Sandeep Sharma");
+  };
+  return (
+    <React.Fragment>
+      <h3>{initialName}</h3>
+      <button type="button" className="btn" onClick={clickEvent}>
+        Click here Change Name
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
