d29ce2f1400d358c6f419821ce1bcd1df25c6ab0
Sandy_undefined <08sandysk@gmail.com>
2020-12-31 12:50:16 +0530

    short circuit

--- Advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ Advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
-
-  return <h2>short circuit</h2>;
+  const [text, setText] = useState("Sandeep");
+  const firstValue = text || "hello world"; // OR
+  const secondValue = text && "hello world"; // AND
+  return (
+    <>
+      <h1>{text || "Sandeep Sharma"}</h1>
+      <h1>{text && "Hello world"}</h1>
+      {/* <h1>First Value: {firstValue}</h1>
+      <h1>Second Value: {secondValue}</h1> */}
+    </>
+  );
 };
 
 export default ShortCircuit;
